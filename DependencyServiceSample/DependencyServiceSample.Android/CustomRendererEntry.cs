cd339238398066e7ef81c1fb5ac29d5c5fd3883a
Abraham Morillo <abrahammorillo777@gmail.com>
2020-03-19 13:32:18 -0400

    CustomRenderer and effects

---
+++ DependencyServiceSample/DependencyServiceSample.Android/CustomRendererEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using DependencyServiceSample.Droid;
+using DependencyServiceSample;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using DependencyServiceSample.Controls;
+using Android.Graphics.Drawables;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(CustomRendererEntry))]
+namespace DependencyServiceSample.Droid
+{
+    public class CustomRendererEntry: EntryRenderer
+    {
+        public CustomRendererEntry(Context context) : base(context)
+        {
+            AutoPackage = false;
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);                
+            }
+        }
+    }
+}
